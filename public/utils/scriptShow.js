017f91caaa154da6b7b007154319b86b1fd9ec3f
Kayo Renato <kayorenatocontato@gmail.com>
2020-03-28 18:34:40 -0300

    fix: change show / hide text on recipe page

--- public/utils/scriptShow.js
+++ public/utils/scriptShow.js
@@ -3,8 +3,8 @@ const links = document.querySelectorAll('h3 a')
 
 for(let link of links){
   link.addEventListener('click', ()=>{
-    const elemShowHide = checkTopic(link)
-    elemShowHide.classList.toggle('hide')
+    checkText(link)
+    checkTopic(link).classList.toggle('hide')
   })
   
 }
@@ -12,4 +12,12 @@ for(let link of links){
 function checkTopic(link){
   const topic = link.classList.value
   return (document.querySelector(`.${topic} ul`) || document.querySelector(`.${topic} p`))
+}
+
+function checkText(link){
+  if(link.innerText == 'Mostrar'){
+    return link.innerText = 'Esconder'
+  } else {
+    return link.innerText = 'Mostrar'
+  }
 }
